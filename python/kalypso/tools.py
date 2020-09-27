957ba38df213ec044e419530defcb7f3659081de
Robert Podschwadt <robertpodschwadt@gmail.com>
2020-09-26 19:57:03 -0500

    a bit of clean up

--- python/kalypso/tools.py
+++ python/kalypso/tools.py
@@ -12,7 +12,7 @@ def layer_outputs( input_tensor, layers, x ):
     except Exception:
         outputs = [ layers.output ]
     # evaluation function
-    print( K.learning_phase() )
+    # print( K.learning_phase() )
     functor = K.function( input_tensor, outputs )
 
     # run the function
