d514caa1d9e9dbc449a8157e8f66a5361b7ffa91
Robert Podschwadt <robertpodschwadt@gmail.com>
2020-10-18 00:00:51 -0500

    fixed comments in config

--- src/tools/Config.cpp
+++ src/tools/Config.cpp
@@ -45,7 +45,7 @@ void Config::readConfig(){
 		str.erase( remove_if( str.begin(), str.end(), isspace ), str.end() );
 
 		// is it a comment
-		if( boost::starts_with( "#", str ) )
+		if( boost::starts_with( str, "#" ) )
 			continue;
 
 		// is it an empty line, or is the line too short?
